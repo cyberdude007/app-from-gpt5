d10cbbfbfd7ccc2aafcebf19037dce8babdbf685
cyberdude007 <65081347+cyberdude007@users.noreply.github.com>
2025-08-10 03:52:13 +0530

    Update build.gradle.kts
    updated the kotlinOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
+    kotlinOptions {
+    jvmTarget = "17"
+    freeCompilerArgs += listOf(
+        "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        )
+    }
+
 
     buildFeatures { compose = true }
 
